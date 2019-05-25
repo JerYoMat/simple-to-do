d4b661f740ea964e87e03f9b047ae2e94ff611e0
JerYoMat <jeremy.emata@gmail.com>
2019-05-25 19:35:29 -0400

    finished todo app;

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Heading from './components/Heading';
 import Input from './components/Input';
 import Button from './components/Button';
 import TodoList from './components/TodoList';
+import TabBar from './components/TabBar'
 
 let todoIndex = 0
 
@@ -16,7 +17,9 @@ class App extends Component {
       todos: [],
       type: 'All'
     }
-   
+    this.toggleComplete = this.toggleComplete.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this)
+    this.setType = this.setType.bind(this)
     this.submitTodo = this.submitTodo.bind(this)
   }
 
@@ -37,11 +40,33 @@ class App extends Component {
       console.log('State: ', this.state)
     })
   }
+  deleteTodo (todoIndex) {
+    let { todos } = this.state
+    todos = this.state.todos.filter((todo) => {
+      return todo.todoIndex !== todoIndex
+    })
+    this.setState({ todos })
+  }
+
+  toggleComplete (todoIndex) {
+    let { todos } = this.state
+    todos.forEach((todo) => {
+      if (todo.todoIndex === todoIndex) {
+        todo.complete = !todo.complete
+      }
+    })
+    this.setState({ todos })
+  }
+
+  setType (type) {
+    this.setState({ type })
+  }
+
 
 
 
   render () {
-    const { todos, inputValue } = this.state
+    const { todos, inputValue, type } = this.state
     return (
       <View
         style={styles.container}>
@@ -52,10 +77,17 @@ class App extends Component {
           <Input
             inputValue={inputValue}
             inputChange={(text) => this.inputChange(text)} />
-     <TodoList todos={todos} />
+     <TodoList
+            type={type}
+            toggleComplete={this.toggleComplete}
+            deleteTodo={this.deleteTodo}
+            todos={todos} />
           <Button
             submitTodo={this.submitTodo} />
         </ScrollView>
+        <TabBar
+          type={type}
+          setType={this.setType} />
       </View>
     )
   }
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { View } from 'react-native';
-import Todo from './Todo';
 
-const TodoList = ({ todos }) => {
+import React from 'react'
+import { View } from 'react-native'
+import Todo from './Todo'
+
+const TodoList = ({ type, todos, deleteTodo, toggleComplete }) => {
+  const getVisibleTodos = (todos, type) => {
+    switch (type) {
+      case 'All':
+        return todos
+      case 'Complete':
+        return todos.filter((t) => t.complete)
+      case 'Active':
+        return todos.filter((t) => !t.complete)
+    }
+  }
+
+  todos = getVisibleTodos(todos, type)
+
   todos = todos.map((todo, i) => {
     return (
       <Todo
         key={i}
+        deleteTodo={deleteTodo}
+        toggleComplete={toggleComplete}
         todo={todo} />
     )
   })
+
   return (
     <View>
       {todos}
@@ -17,4 +34,4 @@ const TodoList = ({ todos }) => {
   )
 }
 
-export default TodoList;
+export default TodoList

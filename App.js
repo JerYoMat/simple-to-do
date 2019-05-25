4c46f8719e3aeaf9517f2de81c177c4dd97819ce
JerYoMat <jeremy.emata@gmail.com>
2019-05-25 18:26:02 -0400

    added input with submit button and event handlers

--- App.js
+++ App.js
@@ -1,39 +1,69 @@
-import React, { Component } from 'react'
-import { View, ScrollView, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import Heading from './components/Heading';
+import Input from './components/Input';
+import Button from './components/Button';
+
+
+let todoIndex = 0
 
 class App extends Component {
-  constructor() {
+
+  constructor () {
     super()
     this.state = {
       inputValue: '',
       todos: [],
       type: 'All'
     }
+   
+    this.submitTodo = this.submitTodo.bind(this)
   }
-  
-  render() {
+
+  inputChange (inputValue) {
+    this.setState({ inputValue })
+  }
+
+  submitTodo () {
+    if (this.state.inputValue.match(/^\s*$/)) return
+    let todo = { title: this.state.inputValue, todoIndex: todoIndex, complete: false }
+    todoIndex++
+    this.state.todos.push(todo)
+    this.setState({ todos: this.state.todos, inputValue: '' }, () => {
+      console.log('State: ', this.state)
+    })
+  }
+
+
+
+  render () {
+    const { todos, inputValue } = this.state
     return (
-      <View style={styles.container}>
-      <ScrollView
-       keyboardShouldPersistTaps='always'
-       style={styles.content}>
-        <Heading />
-      </ScrollView>
-    </View>
+      <View
+        style={styles.container}>
+        <ScrollView
+          keyboardShouldPersistTaps='always'
+          style={styles.content}>
+          <Heading />
+          <Input
+            inputValue={inputValue}
+            inputChange={(text) => this.inputChange(text)} />
+          <Button
+            submitTodo={this.submitTodo} />
+        </ScrollView>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5'
+    backgroundColor: '#f5f5f5',
+    flex: 1
   },
   content: {
-    flex: 1,
-    paddingTop: 60
+    flex: 1
   }
 })
 
-export default App;
+export default App
--- components/Input.js
+++ components/Input.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { View, TextInput, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
 
-const Input = () => (
+const Input = ({ inputValue, inputChange }) => (  
   <View style={styles.inputContainer}>
     <TextInput
+      value={inputValue}
       style={styles.input}
       placeholder='What needs to be done?'
       placeholderTextColor='#CACACA'
-      selectionColor='#666666' />
+      selectionColor='#666666'
+      onChangeText={inputChange} />    
   </View>
 )
-
 const styles = StyleSheet.create({
   inputContainer: {
     marginLeft: 20,
